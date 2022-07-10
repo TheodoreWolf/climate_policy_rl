44fe157fa395e60925cc123ecd7f931f7a55d67c
Theo_Wolf <theo.wolf@successdata.ai>
2022-07-10 13:36:32 +0100

    update

---
+++ classes/Learning/Lightning.py
@@ -0,0 +1,35 @@
+import os
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+import networks as net
+import wandb
+from utils import ReplayBuffer
+
+
+class DQNLightning(pl.LightningModule):
+    def __init__(self,
+                 config: dict,
+                 model: nn.Module,
+                 env: object,
+                 name: str):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.network = model
+        wandb.watch(self.network)
+        self.__name__ = name
+        self.env = env
+        self.buffer = ReplayBuffer(config['BUFFER_SIZE'])
+
+        wandb.init(project="AYS_learning", entity="climate_policy_optim", name=name, config=config)
+
+    def forward(self, state) -> Tensor:
+        return self.network(state)
+
+
+
