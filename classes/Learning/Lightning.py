819a17270122776e6d0272bd88a66be6b01a5568
Theo_Wolf <theo.wolf@successdata.ai>
2022-07-13 13:59:08 +0100

    learning loop

--- classes/Learning/Lightning.py
+++ classes/Learning/Lightning.py
@@ -1,39 +1,155 @@
 import os
+from collections import OrderedDict, deque, namedtuple
+from typing import Iterator, List, Tuple
+
+import gym
+import numpy as np
+import pandas as pd
+import seaborn as sn
 import torch
-from torch import nn, Tensor
-import torch.nn.functional as F
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
-import pytorch_lightning as pl
-import networks as net
-import wandb
-from utils import ReplayBuffer
+from IPython.core.display import display
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import CSVLogger
+from torch import Tensor, nn
+from torch.optim import Adam, Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import IterableDataset
 
+from .utils import ReplayBuffer
+from .networks import DuellingNet
 
-class DQNLightning(pl.LightningModule):
-    def __init__(self,
-                 config: dict,
-                 model: nn.Module,
-                 env: object,
-                 name: str):
+class DQNLightning(LightningModule):
+    """Basic DQN Model."""
+
+    def __init__(
+        self,
+        batch_size: int = 16,
+        lr: float = 1e-2,
+        env: str = "CartPole-v0",
+        gamma: float = 0.99,
+        sync_rate: int = 10,
+        replay_size: int = 1000,
+        warm_start_size: int = 1000,
+        eps_last_frame: int = 1000,
+        eps_start: float = 1.0,
+        eps_end: float = 0.01,
+        episode_length: int = 200,
+        warm_start_steps: int = 1000,
+    ) -> None:
+
         super().__init__()
         self.save_hyperparameters()
 
-        self.network = model
-        wandb.watch(self.network)
-        self.__name__ = name
-        self.env = env
-        self.buffer = ReplayBuffer(config['BUFFER_SIZE'])
+        self.env = eval(self.hparams.env).__init__(dt=DT, reward_type=REWARD_TYPE)
+        obs_size = len(self.env.observation_space)
+        n_actions = len(self.action_space)
 
-        wandb.init(project="AYS_learning", entity="climate_policy_optim", name=name, config=config)
+        self.net = None
 
-    def forward(self, state) -> Tensor:
-        return self.network(state)
+        self.buffer = ReplayBuffer(self.hparams.replay_size)
+        self.total_reward = 0
+        self.episode_reward = 0
+        self.populate(self.hparams.warm_start_steps)
 
-    def training_step(self, *args, **kwargs) -> float:
-        pass
+    def forward(self, x: Tensor) -> Tensor:
+        """Passes in a state x through the network and gets the q_values of each action as an output.
 
+        Args:
+            x: environment state
 
+        Returns:
+            q values
+        """
+        output = self.net(x)
+        return output
 
+    def dqn_mse_loss(self, batch: Tuple[Tensor, Tensor]) -> Tensor:
+        """Calculates the mse loss using a mini batch from the replay buffer.
 
+        Args:
+            batch: current mini batch of replay data
+
+        Returns:
+            loss
+        """
+        states, actions, rewards, dones, next_states = batch
+
+        state_action_values = self.net(states).gather(1, actions.long().unsqueeze(-1)).squeeze(-1)
+
+        with torch.no_grad():
+            next_state_values = self.target_net(next_states).max(1)[0]
+            next_state_values[dones] = 0.0
+            next_state_values = next_state_values.detach()
+
+        expected_state_action_values = next_state_values * self.hparams.gamma + rewards
+
+        return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+    def get_epsilon(self, start: int, end: int, frames: int) -> float:
+        if self.global_step > frames:
+            return end
+        return start - (self.global_step / frames) * (start - end)
+
+    def training_step(self, batch: Tuple[Tensor, Tensor], nb_batch) -> OrderedDict:
+        """Carries out a single step through the environment to update the replay buffer. Then calculates loss
+        based on the minibatch recieved.
+
+        Args:
+            batch: current mini batch of replay data
+            nb_batch: batch number
+
+        Returns:
+            Training loss and log metrics
+        """
+        device = self.get_device(batch)
+        epsilon = self.get_epsilon(self.hparams.eps_start, self.hparams.eps_end, self.hparams.eps_last_frame)
+        self.log("epsilon", epsilon)
+
+        # step through environment with agent
+        reward, done = self.agent.play_step(self.net, epsilon, device)
+        self.episode_reward += reward
+        self.log("episode reward", self.episode_reward)
+
+        # calculates training loss
+        loss = self.dqn_mse_loss(batch)
+
+        if done:
+            self.total_reward = self.episode_reward
+            self.episode_reward = 0
+
+        # Soft update of target network
+        if self.global_step % self.hparams.sync_rate == 0:
+            self.target_net.load_state_dict(self.net.state_dict())
+
+        self.log_dict(
+            {
+                "reward": reward,
+                "train_loss": loss,
+            }
+        )
+        self.log("total_reward", self.total_reward, prog_bar=True)
+        self.log("steps", self.global_step, logger=False, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self) -> List[Optimizer]:
+        """Initialize Adam optimizer."""
+        optimizer = Adam(self.net.parameters(), lr=self.hparams.lr)
+        return optimizer
+
+    def __dataloader(self) -> DataLoader:
+        """Initialize the Replay Buffer dataset used for retrieving experiences."""
+        dataset = RLDataset(self.buffer, self.hparams.episode_length)
+        dataloader = DataLoader(
+            dataset=dataset,
+            batch_size=self.hparams.batch_size,
+        )
+        return dataloader
+
+    def train_dataloader(self) -> DataLoader:
+        """Get train loader."""
+        return self.__dataloader()
+
+    def get_device(self, batch) -> str:
+        """Retrieve device currently being used by minibatch."""
+        return batch[0].device.index if self.on_gpu else "cpu"
--- classes/learning_loop.py
+++ classes/learning_loop.py
@@ -3,6 +3,7 @@ import numpy as np
 from importlib import reload
 from tqdm import tqdm
 
+import matplotlib.pyplot as plt
 import wandb
 import torch
 
@@ -10,122 +11,116 @@ import Envs.AYS.AYS_Environment as ays
 import Learning.agents as ag
 import Learning.utils as utils
 
-AGENT = "PPO"
-UPDATE_ONLINE = False
-BATCH_SIZE = 128
-MAX_EPISODES = 2000
-RANDOM_EXPERIENCE = 0
-LEARNING_RATE = 3e-4
-BUFFER_SIZE = 10000
-REWARD_TYPE = 'policy_cost'
-DT = 1
-SCHEDULER = (False, 1000, 0.5)
-SEED = 0
-MAX_STEPS = 600
 
-# config
-default_config = {
-    "learning_rate": LEARNING_RATE,
-    "max_episodes": MAX_EPISODES,
-    "batch_size": BATCH_SIZE,
-    "online_updating": UPDATE_ONLINE,
-    "random_experience": RANDOM_EXPERIENCE,
-    "buffer_size": BUFFER_SIZE,
-    "reward_type": REWARD_TYPE,
-    "dt": DT,
-    "scheduler": SCHEDULER,
-    "seed": SEED
-}
+def learning_loop(AGENT="PPO",
+                  UPDATE_ONLINE=False,
+                  BATCH_SIZE=128,
+                  MAX_EPISODES=2000,
+                  RANDOM_EXPERIENCE=0,
+                  LEARNING_RATE=3e-4,
+                  BUFFER_SIZE=10000,
+                  REWARD_TYPE='PB',
+                  DT=1,
+                  SCHEDULER=(False, 1000, 0.5),
+                  SEED=0,
+                  MAX_STEPS=600,
+                  DISCOUNT=0.99,
+                  MAX_FRAMES=1e5):
+    config = {
+        "learning_rate": LEARNING_RATE,
+        "max_episodes": MAX_EPISODES,
+        "batch_size": BATCH_SIZE,
+        "online_updating": UPDATE_ONLINE,
+        "random_experience": RANDOM_EXPERIENCE,
+        "buffer_size": BUFFER_SIZE,
+        "reward_type": REWARD_TYPE,
+        "dt": DT,
+        "scheduler": SCHEDULER,
+        "seed": SEED,
+        "discount": DISCOUNT,
+    }
+    # seeds
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
 
-class LearningLoop:
-    def __init__(self, config=None):
+    # Environment
+    reload(ays)
+    env = ays.AYS_Environment(discount=DISCOUNT, dt=DT, reward_type=REWARD_TYPE)
+    state_dim = len(env.observation_space)
+    action_dim = len(env.action_space)
 
-        if config is None:
-            config = default_config
-        # seeds
-        random.seed(SEED)
-        np.random.seed(SEED)
-        torch.manual_seed(SEED)
+    # Agent
+    reload(ag)
+    memory = utils.ReplayBuffer(BUFFER_SIZE)
+    agent = eval("ag." + AGENT)(state_dim, action_dim, gamma=DISCOUNT, alpha=LEARNING_RATE)
 
-        # Environment
-        reload(ays)
-        env = ays.AYS_Environment(dt=DT, reward_type=REWARD_TYPE)
-        state_dim = len(env.observation_space)
-        action_dim = len(env.action_space)
+    # Scheduler
+    scheduler = torch.optim.lr_scheduler.StepLR(agent.optimizer, step_size=SCHEDULER[1], gamma=SCHEDULER[2])
 
-        # Agent
-        reload(ag)
-        memory = utils.ReplayBuffer(BUFFER_SIZE)
-        agent = eval("ag." + AGENT)
-        agent = agent.__init__(state_dim, action_dim, alpha=LEARNING_RATE)
+    # WandB naming
+    group = REWARD_TYPE
+    job_type = AGENT + "_" + str(LEARNING_RATE) + "_" + str(DISCOUNT)
+    name = "batch" + str(BATCH_SIZE) + "_buffer" + str(BUFFER_SIZE) + "_seed" + str(SEED)
+    wandb.init(group=group,
+               job_type=job_type,
+               config=config,
+               name=name,
+               entity="climate_policy_optim",
+               project="AYS_learning")
 
-        # Scheduler
-        scheduler = torch.optim.lr_scheduler.StepLR(agent.optimizer, step_size=SCHEDULER[1], gamma=SCHEDULER[2])
+    rewards = []
+    mean_rewards = []
+    frame_idx = 0
 
-        group = AGENT + "_" + REWARD_TYPE + "_" + str(LEARNING_RATE)
-        job_type = "batch" + str(BATCH_SIZE) + "_buffer" + str(BUFFER_SIZE) + "_seed" + str(SEED)
-        wandb.init(group=group,
-                   config=config,
-                   job_type=job_type,
-                   entity="climate_policy_optim",
-                   project="AYS_learning")
+    for episodes in range(MAX_EPISODES):
+        state = env.reset()
+        episode_reward = 0
 
-        rewards = []
-        mean_rewards = []
-        frame_idx = 0
+        for i in range(MAX_STEPS):
+            if episodes > RANDOM_EXPERIENCE:
+                action = agent.get_action(state)
+            else:
+                action = np.random.choice(action_dim)
 
-        for episodes in tqdm(range(MAX_EPISODES)):
-            state = env.reset()
-            episode_reward = 0
-            done = False
+            next_state, reward, done, = env.step(action)
 
-            for i in range(MAX_STEPS):
-                if episodes > RANDOM_EXPERIENCE:
-                    action = agent.get_action(state)
-                else:
-                    action = np.random.choice(action_dim)
+            episode_reward += reward
 
-                next_state, reward, done, = env.step(action)
-
-                if done:
-                    reward += env.calculate_expected_final_reward(agent.gamma, MAX_STEPS, current_step=i)
-                    # if env._good_final_state():
-                    #     reward += 100
-                episode_reward += reward
-
-                if UPDATE_ONLINE:
-                    loss = agent.online_update((state, action, reward, next_state, done))
-
-                    wandb.log({'loss': loss})
-                else:
-                    memory.push(state, action, reward, next_state, done)
-                    if memory.__len__() > BATCH_SIZE:
-                        sample = memory.sample(BATCH_SIZE)
-                        loss = agent.update(sample)
-                        wandb.log({'loss': loss})
-
-                state = next_state
-                frame_idx += 1
-                if done:
-                    break
-
-            rewards.append(episode_reward)
-            mean_rewards.append(np.mean(rewards[-50:]))
-
-            # if not UPDATE_ONLINE:
-            if SCHEDULER[0]:
-                scheduler.step()
-
-            wandb.log({'episode_reward': episode_reward})
-            if episodes % 200 == 0:
-                utils.plot_test_trajectory(env, agent)
+            if UPDATE_ONLINE:
+                loss = agent.online_update((state, action, reward, next_state, done))
+                wandb.log({'loss': loss})
 
             else:
-                if episodes % 10 == 0:
-                    utils.plot(frame_idx, mean_rewards)
-                    if episodes % 30 == 0:
-                        utils.plot_test_trajectory(env, agent)
+                memory.push(state, action, reward, next_state, done)
+                if memory.__len__() > BATCH_SIZE:
+                    sample = memory.sample(BATCH_SIZE)
+                    loss = agent.update(sample)
+                    wandb.log({'loss': loss})
 
-        wandb.run.summary["mean_reward"] = np.mean(rewards)
-        wandb.run.summary["top_reward"] = max(rewards)
-        wandb.finish()
+            state = next_state
+            frame_idx += 1
+            if done:
+                break
+
+        rewards.append(episode_reward)
+        mean_rewards.append(np.mean(rewards[-50:]))
+
+        wandb.log({'episode_reward': episode_reward})
+        print("Episode:", episodes, "reward:", round(episode_reward),"|| Final State ",  env._which_final_state().name)
+
+        if not UPDATE_ONLINE and SCHEDULER[0]:
+            scheduler.step()
+
+        if frame_idx > MAX_FRAMES:
+            break
+
+    wandb.run.summary["mean_reward"] = np.mean(rewards)
+    wandb.run.summary["top_reward"] = max(rewards)
+    wandb.finish()
+    utils.plot_test_trajectory(env, agent)
+
+
+if __name__ == "__main__":
+    learning_loop(AGENT="DuellingDQN",
+                  SEED=1)
